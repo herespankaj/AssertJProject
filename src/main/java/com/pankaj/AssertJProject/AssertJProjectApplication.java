b8f1abaaff0a6c8f38eaa200337c2382e8ea90f0
pankaj <herespankaj@gmail.com>
2020-12-18 11:20:58 +0000

    First Commit made to AssertJProject

---
+++ src/main/java/com/pankaj/AssertJProject/AssertJProjectApplication.java
@@ -0,0 +1,86 @@
+package com.pankaj.AssertJProject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+
+@SpringBootApplication
+public class AssertJProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssertJProjectApplication.class, args);
+		
+		List<Employee> le = new ArrayList<>();
+		
+		le.add(new Employee(1, "Pankaj", 77.20));
+		le.add(new Employee(2, "Rashmi", 67.20));
+		le.add(new Employee(3, "Nishant", 37.20));
+		le.add(new Employee(4, "Pinku", 87.20));
+		le.add(new Employee(5, "Nilu", 57.20)); 
+		
+		assertThat(le.get(1).getName()).isEqualTo("Pankaj"); 
+		assertThat(le.get(3).getName()).isEqualTo("Pankaj"); 
+
+	}
+	
+	
+	
+	
+
+}
+
+class Employee { 
+	
+	private int id;
+	private String name; 
+	private double weight;
+	
+	public Employee() {
+		super();
+	}
+
+	public Employee(int id, String name, double weight) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.weight = weight;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", weight=" + weight + "]";
+	} 
+	
+	
+	
+}
---
+++ src/test/java/com/pankaj/AssertJProject/AssertJProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pankaj.AssertJProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssertJProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
